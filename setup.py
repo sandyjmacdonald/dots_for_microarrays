2cd012f4465db6ea39c7578c97528b030b3e6a1e
sandyjmacdonald <sandyjmacdonald@gmail.com>
2015-06-23 16:23:01 +0100

    Updated setup.py before pushing v0.1.1 to PyPi

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='dots_for_microarrays',
-    version='0.1.0',
+    version='0.1.1',
     author='Sandy Macdonald',
     author_email='sandyjmacdonald@gmail.com',
     packages=['dots_backend', 'dots_tests'],
     scripts=['dots_scripts/dots_workflow.py'],
     url='https://github.com/sandyjmacdonald/dots_for_microarrays',
-    download_url='https://github.com/sandyjmacdonald/dots_for_microarrays/tarball/0.1.0',
+    download_url='https://github.com/sandyjmacdonald/dots_for_microarrays/tarball/v0.1.1',
     license='LICENSE.txt',
     description='Simple analysis of Agilent one-color arrays.',
     keywords = ['bioinformatics', 'genomics', 'microarrays'],

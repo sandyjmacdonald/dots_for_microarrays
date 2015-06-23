375ecd66a24c5aadc13055f694698fbd222682c3
sandyjmacdonald <sandyjmacdonald@gmail.com>
2015-06-23 16:48:53 +0100

    Added some categories to setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,25 @@ from setuptools import setup
 
 setup(
     name='dots_for_microarrays',
-    version='0.1.1',
+    version='0.1.2',
     author='Sandy Macdonald',
     author_email='sandyjmacdonald@gmail.com',
     packages=['dots_backend', 'dots_tests'],
     scripts=['dots_scripts/dots_workflow.py'],
     url='https://github.com/sandyjmacdonald/dots_for_microarrays',
-    download_url='https://github.com/sandyjmacdonald/dots_for_microarrays/tarball/v0.1.1',
-    license='LICENSE.txt',
+    download_url='https://github.com/sandyjmacdonald/dots_for_microarrays/tarball/v0.1.2',
+    license='MIT',
     description='Simple analysis of Agilent one-color arrays.',
     keywords = ['bioinformatics', 'genomics', 'microarrays'],
+    classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: Unix',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: MacOS :: MacOS X',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ],
     install_requires=[
         'numpy',
         'scipy',
